500c74c4376d8104b6bcd2352811d42627d4645b
Brandon <zermelwall@gmail.com>
2021-10-29 10:22:50 +0200

    début api

--- api.py
+++ api.py
@@ -1,15 +1,53 @@
 from typing import Optional
 
-from fastapi import FastAPI
-
-app = FastAPI()
+import pandas as pd
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from flask import Flask, render_template, jsonify
+from flask import request
+import json
+import requests
+
+    
+
+df = pd.read_csv('data/application_train.csv')
+df_score = pd.read_csv('log_reg_baseline.csv')
+
+app = Flask(__name__)
 
 
-@app.get("/client/{item_id}/score")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/api/clients/')
+def clients():
+    args = request.args
+    fields = args['fields']
+    # Si l'utilisateur demande des champs en particulier alors on lui renvoie que cela
+    if fields:
+        fields = fields.split(",")
+    else:
+        fields = df.columns.values
+
+    return jsonify({
+      'status': 'ok', 
+      'data': df[[fields]].to_dict(orient='records')
+    })
+
+@app.route('/api/clients/score')
+def clients():
+    args = request.args
+    identifiant = args['id']
+
+    return jsonify({
+      'status': 'ok', 
+      'data': []
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
